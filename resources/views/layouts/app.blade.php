41e28496e4006a171070c63fdb3b6b4371c26a53
Mohammed Alkabsh <jaksws@gmail.com>
2025-04-25 16:45:23 +0300

    Remove Bootstrap JS inclusion from app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -355,8 +355,6 @@
         <script src="{{ asset('js/dark-mode.js') }}"></script>
     @endif
     
-    <!-- بوتستراب JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     @stack('scripts')
 </body>
 </html>
