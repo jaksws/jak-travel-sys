c402486597aafcd1ccb0144b47446375f0dd7b4a
Mohammed Alkabsh <jaksws@gmail.com>
2025-05-02 19:16:57 +0300

    Add check for existing "documents" table before creating it
    * Drop the "documents" table if it already exists before creating it in the migration script

--- database/migrations/2024_04_10_000002_create_documents_table.php
+++ database/migrations/2024_04_10_000002_create_documents_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('documents')) {
+            Schema::drop('documents');
+        }
+
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
             $table->string('name');
