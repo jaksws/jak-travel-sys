5b782a8138c99f676f721bc8ec4f8676d5fcd038
Mohammed Alkabsh <jaksws@gmail.com>
2025-05-20 03:15:05 +0300

    fix(auth): escape validation message for missing license number in agency registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -94,8 +94,7 @@ class RegisterController extends Controller
             // Throwing a ValidationException here is a last-resort safeguard.
             if (empty($data['license_number'])) {
                 throw \Illuminate\Validation\ValidationException::withMessages([
-                    // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-                    'license_number' => __('رقم الترخيص مطلوب لتسجيل الوكالة.')
+                    'license_number' => e(__('رقم الترخيص مطلوب لتسجيل الوكالة.'))
                 ]);
             }
             $agency = Agency::create([
